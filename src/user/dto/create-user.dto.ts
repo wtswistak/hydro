24e755054f1e55d7b7d28fef9e4ca6c2af068e48
unknown <wtswistak@gmail.com>
2025-01-03 23:32:03 +0100

    feat: create user

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString, Min, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+    if (user) {
+      throw new Error('User already exists');
+    }
+    return this.prisma.user.create({
+      data: {
+        email: createUserDto.email,
+        password: createUserDto.password,
+      },
+    });
+  }
+}
