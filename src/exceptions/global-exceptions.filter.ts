62521d9ba47e961b94c22841eeb6d313db7248e6
wtswistak <wtswistak@gmail.com>
2025-01-05 21:42:49 +0100

    feat: exceptions filter

---
+++ src/exceptions/global-exceptions.filter.ts
@@ -0,0 +1,42 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  Logger,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger('Error');
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const exceptionResponse =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : { message: 'Internal server error' };
+
+    const message =
+      (exceptionResponse as any).message || 'Internal server error';
+    const finalMessage = Array.isArray(message) ? message[0] : message;
+    this.logger.error(finalMessage);
+    this.logger.error(exception.stack);
+
+    response.status(status).json({
+      statusCode: status,
+      message: finalMessage,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './exceptions/global-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  const logger = new Logger('Main');
+  app.useLogger(logger);
+
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(
+    `Server is running on http://localhost:${process.env.PORT ?? 3000}`,
+  );
 }
 bootstrap();
