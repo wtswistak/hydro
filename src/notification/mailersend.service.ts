25aad32d0eeb28b441d9848cf0d8e519a5f82a7d
wtswistak <wtswistak@gmail.com>
2025-01-25 13:25:58 +0100

    feat: mailersend service

---
+++ src/notification/mailersend.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { EmailParams, MailerSend, Recipient, Sender } from 'mailersend';
+import { AppConfigService } from 'src/config/app-config.service';
+
+@Injectable()
+export class MailersendService {
+  private readonly logger = new Logger(MailersendService.name);
+  constructor(private readonly configService: AppConfigService) {}
+
+  async sendEmail({ receipent, subject, html }) {
+    const mailerSend = new MailerSend({
+      apiKey: this.configService.mailersendApiKey,
+    });
+    const sentFrom = new Sender(
+      `barack@${this.configService.mailersendDomain}`,
+      'Barack Obama',
+    );
+    const receipents = [new Recipient(receipent)];
+    const emailParams = new EmailParams()
+      .setFrom(sentFrom)
+      .setTo(receipents)
+      .setSubject(subject)
+      .setHtml(html);
+    try {
+      await mailerSend.email.send(emailParams);
+      this.logger.log(`Email sent to ${receipent}, subjeect: ${subject}`);
+    } catch (error) {
+      this.logger.error(`Failed to send email to ${receipent}`, error);
+    }
+  }
+}
