59bdb56dab14ebc1df1cef0174541a1631dcaf4f
wtswistak <wtswistak@gmail.com>
2025-01-29 23:19:20 +0100

    feat: type cryptocurrency

--- src/coingecko/coingecko.controller.ts
+++ src/coingecko/coingecko.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CoingeckoService } from './coingecko.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('coingecko')
 export class CoingeckoController {
   constructor(private readonly coingeckoService: CoingeckoService) {}
 
   @Get('cryptocurrencies')
+  @UseGuards(AuthGuard('jwt'))
   getCryptocurrenciesList() {
-    return this.coingeckoService.getCryptocurrenciesList();
+    return this.coingeckoService.getCryptocurrencies();
   }
 }
--- src/coingecko/coingecko.service.ts
+++ src/coingecko/coingecko.service.ts
@@ -1,16 +1,17 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
+import { ICryptocurrency } from './interface/cryptocurrency.interface';
 
 @Injectable()
 export class CoingeckoService {
   private readonly logger = new Logger(CoingeckoService.name);
   constructor(private readonly httpService: HttpService) {}
 
-  async getCryptocurrenciesList() {
+  async getCryptocurrencies(): Promise<ICryptocurrency[]> {
     try {
       const { data } = await firstValueFrom(
-        this.httpService.get(
+        this.httpService.get<ICryptocurrency[]>(
           'coins/markets?vs_currency=usd&per_page=10&page=1',
         ),
       );
