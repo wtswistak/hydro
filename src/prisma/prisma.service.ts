5d6bdf8ff377576f9f791daa916851736941fb86
wtswistak <wtswistak@gmail.com>
2025-02-11 20:20:08 +0100

    feat: prisma soft delete implementation

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,40 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super();
+    this.$extends({
+      model: {
+        $allModels: {
+          async delete<M, A>(
+            this: M,
+            where: Prisma.Args<M, 'delete'>,
+          ): Promise<Prisma.Result<M, A, 'update'>> {
+            const context = Prisma.getExtensionContext(this);
+
+            if (!('deletedAt' in (context as any).fields)) {
+              return (context as any).delete(where);
+            }
+
+            return (context as any).update({
+              ...where,
+              data: { deletedAt: new Date() },
+            });
+          },
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
+
   async onModuleDestroy() {
     await this.$disconnect();
   }
