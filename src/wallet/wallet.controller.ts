8c5aea079c51e551cce718577559c21410c87b4b
wtswistak <wtswistak@gmail.com>
2025-02-12 22:40:36 +0100

    feat: create wallet endpoint

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { WalletService } from './wallet.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Wallet } from '@prisma/client';
 
 @Controller('wallet')
-export class WalletController {}
+export class WalletController {
+  constructor(private readonly walletService: WalletService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  createWallet(@Req() req: any): Promise<Wallet> {
+    const userId = req.user.id;
+    return this.walletService.createWallet(userId);
+  }
+}
--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { BlockchainService } from 'src/blockchain/blockchain.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CryptoService } from './crypto.service';
+import { Wallet } from '@prisma/client';
 
 @Injectable()
 export class WalletService {
@@ -12,7 +13,8 @@ export class WalletService {
     private cryptoService: CryptoService,
   ) {}
 
-  async createWallet(userId: number) {
+  async createWallet({ userId }: { userId: number }): Promise<Wallet> {
+    this.logger.log(`Creating wallet for user with id: ${userId}`);
     const blockchainWallet = this.blockchainService.createWallet();
     const encryptedPrivateKey = this.cryptoService.encrypt({
       privateKey: blockchainWallet.privateKey,
