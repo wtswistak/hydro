8b447c73771a49a88f135c1157ea21fe97a171a5
wtswistak <wtswistak@gmail.com>
2025-02-27 20:30:39 +0100

    fix: get access token

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.accessToken,
+      secretOrKey: async () => configService.accessToken,
     });
   }
 
