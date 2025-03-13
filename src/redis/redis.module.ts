a5a51ae5abf97e5924902c24d83a3fc682566665
wtswistak <wtswistak@gmail.com>
2025-03-13 22:10:45 +0100

    fix: redis config

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,31 +1,26 @@
-import { DynamicModule, Global, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Global, Module } from '@nestjs/common';
 import Redis, { RedisOptions } from 'ioredis';
+import { AppConfigModule } from 'src/config/app-config.module';
 import { AppConfigService } from 'src/config/app-config.service';
 
 @Global()
-@Module({})
-export class RedisModule {
-  static forRootAsync(): DynamicModule {
-    return {
-      module: RedisModule,
-      imports: [ConfigModule],
-      providers: [
-        {
-          provide: 'REDIS_CLIENT',
-          useFactory: async (configService: AppConfigService) => {
-            const option: RedisOptions = {
-              host: configService.redisHost,
-              port: configService.redisPort,
-            };
-            const client = new Redis(option);
-            await client.connect();
-            return client;
-          },
-          inject: [AppConfigService],
-        },
-      ],
-      exports: ['REDIS_CLIENT'],
-    };
-  }
-}
+@Module({
+  imports: [AppConfigModule],
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: async (configService: AppConfigService) => {
+        const option: RedisOptions = {
+          host: configService.redisHost,
+          port: configService.redisPort,
+        };
+        const client = new Redis(option);
+
+        return client;
+      },
+      inject: [AppConfigService],
+    },
+  ],
+  exports: ['REDIS_CLIENT'],
+})
+export class RedisModule {}
