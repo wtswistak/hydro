2ddbf2bc3f06ad59f055837d9e63297240b10ae0
wtswistak <wtswistak@gmail.com>
2025-04-09 20:19:40 +0200

    refactor(webhook): import modules

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
 import { TransactionModule } from 'src/transaction/transaction.module';
+import { WalletModule } from 'src/wallet/wallet.module';
+import { BalanceService } from 'src/balance/balance.service';
 
 @Module({
-  imports: [TransactionModule],
+  imports: [TransactionModule, WalletModule],
   controllers: [WebhookController],
-  providers: [WebhookService],
+  providers: [WebhookService, BalanceService],
 })
 export class WebhookModule {}
