c30daeceda433d54ad1bfcb85be895f2fb24c192
wtswistak <wtswistak@gmail.com>
2025-05-20 21:43:04 +0200

    fix: delete onnx

--- src/fee-prediction/fee-prediction.service.ts
+++ src/fee-prediction/fee-prediction.service.ts
@@ -1,21 +1,12 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { formatEther } from 'ethers';
-import * as ort from 'onnxruntime-node';
 import { FeeSnapshotService } from 'src/fee-snapshot/fee-snapshot.service';
 import { FeePrediction } from './interface/get-fee-prediction.dto';
 
 @Injectable()
-export class FeePredictionService implements OnModuleInit {
-  private session!: ort.InferenceSession;
+export class FeePredictionService {
   private readonly logger = new Logger(FeePredictionService.name);
   constructor(private readonly feeSnapshotService: FeeSnapshotService) {}
-
-  async onModuleInit() {
-    this.session = await ort.InferenceSession.create(
-      './src/fee-prediction/ml-models/eth_fee_model_v2.onnx',
-    );
-    this.logger.log('ONNX model loaded successfully ✅');
-  }
 
   async getFeePrediction(steps: number): Promise<FeePrediction[]> {
     let snapshots = await this.feeSnapshotService.getLastFeeSnapshots({
@@ -53,13 +44,7 @@ export class FeePredictionService implements OnModuleInit {
         priorityGap,
       ]);
 
-      const feeds = { float_input: new ort.Tensor('float32', input, [1, 8]) };
-      const results = await this.session.run(feeds);
-      const outName = Object.keys(results)[0];
-      const rawPred = (results[outName].data as Float32Array)[0];
-      const gasPriceGwei = Number(rawPred.toFixed(6));
-
-      const gasPriceWeiBig = BigInt(Math.round(gasPriceGwei * 1e9));
+      const gasPriceWeiBig = BigInt(Math.round(2 * 1e9));
       const gasLimitBig = BigInt(21_000);
       const txCostWeiBig = gasPriceWeiBig * gasLimitBig;
 
@@ -67,14 +52,14 @@ export class FeePredictionService implements OnModuleInit {
 
       forecasts.push({
         minutesAhead: i * 10,
-        gasPriceGwei,
+        gasPriceGwei: 2,
         txCostEth,
       });
 
       // synulacja nowy snapshot
       const synthetic: typeof latest = {
         ...latest,
-        baseFeePerGas: BigInt(Math.round(gasPriceGwei * 1e9)),
+        baseFeePerGas: BigInt(Math.round(2 * 1e9)),
         createdAt: new Date(latest.createdAt.getTime() + 10 * 60 * 1000),
       };
 
