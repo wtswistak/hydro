c27ec256f31fdff62ede75d69ca77fb4d17fdb7b
wtswistak <wtswistak@gmail.com>
2025-05-21 21:46:30 +0200

    fix(auth): remove unused passpord module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { AppConfigService } from 'src/config/app-config.service';
 import { NotificationModule } from 'src/notification/notification.module';
@@ -10,7 +9,6 @@ import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 
 @Module({
   imports: [
-    PassportModule,
     NotificationModule,
     JwtModule.register({
       secret: process.env.ACCESS_TOKEN,
@@ -19,6 +17,5 @@ import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
   ],
   providers: [AuthService, AppConfigService, JwtStrategy, JwtRefreshStrategy],
   controllers: [AuthController],
-  exports: [AuthService],
 })
 export class AuthModule {}
