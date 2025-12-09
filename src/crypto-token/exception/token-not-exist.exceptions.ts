d1e59bdc99ff1312815a0ca66d9c227e2a0df6d8
wtswistak <wtswistak@gmail.com>
2025-12-09 22:15:31 +0100

    feat(exception): add exception for not existing crypto token

---
+++ src/crypto-token/exception/token-not-exist.exceptions.ts
@@ -0,0 +1,12 @@
+import { HttpStatus } from '@nestjs/common';
+import { BaseException } from 'src/common/exception/base-exception';
+
+export class CryptoTokenNotExistException extends BaseException {
+  constructor() {
+    super(
+      'Crypto token does not exist',
+      'CRYPTO_TOKEN_NOT_EXIST',
+      HttpStatus.NOT_FOUND,
+    );
+  }
+}
