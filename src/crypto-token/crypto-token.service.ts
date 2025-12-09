8adfc0b2b34618a719ef446e8ae2cfdaf8288928
wtswistak <wtswistak@gmail.com>
2025-12-09 22:15:00 +0100

    feat(service): implement crypto token service

---
+++ src/crypto-token/crypto-token.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+
+@Injectable()
+export class CryptoTokenService {
+  private readonly logger = new Logger(CryptoTokenService.name);
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getCryptoTokenBySymbol({ symbol }: { symbol: string }) {
+    return this.prisma.cryptoToken.findUnique({
+      where: { symbol },
+    });
+  }
+}
