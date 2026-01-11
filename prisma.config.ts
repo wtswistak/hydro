e1f014208a118ec05ef6172a440933c9af975e9d
wtswistak <wtswistak@gmail.com>
2026-01-11 15:53:01 +0100

    feat(prisma): fix running cli commands/ migrate dev

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,9 @@
-const config = {
-  datasource: {
-    url: process.env.DATABASE_URL
-  }
-};
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
-export default config;
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
