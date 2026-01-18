eb0200a70e5bd792a5e64be6974bae91ae5041e2
wtswistak <wtswistak@gmail.com>
2026-01-18 21:38:31 +0100

    refator(seed): add bitcoin chain in blockchain seed

--- prisma/seeds/blockchain.seed.ts
+++ prisma/seeds/blockchain.seed.ts
@@ -1,16 +1,35 @@
 import { Logger } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, BlockchainType } from '@prisma/client';
 
-const prisma = new PrismaClient();
-
-export async function seedBlockchain() {
+export async function seedBlockchain(prisma: PrismaClient) {
   const logger = new Logger('seed');
   logger.log('Seeding Blokchain...');
-  await prisma.blockchain.create({
-    data: {
-      name: 'Etherum',
-      chainId: 1,
-      nativeSymbol: 'ETH',
-    },
+
+  const ethChain = await prisma.blockchain.findUnique({
+    where: { name: 'Ethereum' },
   });
+  const btcChain = await prisma.blockchain.findUnique({
+    where: { name: 'Bitcoin' },
+  });
+
+  if (!ethChain) {
+    await prisma.blockchain.create({
+      data: {
+        name: 'Ethereum',
+        chainId: 11155111, // Sepolia
+        nativeSymbol: 'ETH',
+        type: BlockchainType.EVM,
+      },
+    });
+  }
+  if (!btcChain) {
+    await prisma.blockchain.create({
+      data: {
+        name: 'Bitcoin',
+        chainId: null,
+        nativeSymbol: 'BTC',
+        type: BlockchainType.BITCOIN,
+      },
+    });
+  }
 }
